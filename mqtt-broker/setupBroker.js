808e36aa2afc530431eda37804b86b93ad37c3f2
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-10-02 00:30:36 +0530

    Mqtt Broker completed
    Wrote the code to connect to the AWS Mqtt Broker using the sdk provided
    by AWS

---
+++ mqtt-broker/setupBroker.js
@@ -0,0 +1,38 @@
+const awsIoT = require('aws-iot-device-sdk');
+const incomingMessageHandler = require('./messageHandler');
+
+var MqttBroker = awsIoT.device({
+	keyPath: './_creds/private.pem.key',
+	certPath: './_creds/certificate.pem.crt',
+	caPath: './_creds/root-CA1.pem',
+	clientId: 'server',
+	host: `${process.env.AWS_IOT_HOST}.iot.${process.env.AWS_IOT_REGION}.amazonaws.com`,
+	region: 'ap-south-1',
+});
+
+MqttBroker.on('message', incomingMessageHandler);
+
+MqttBroker.on('connect', () => {
+	console.log('Connected to AWS IoT!!');
+	MqttBroker.publish(
+		'RAS/Connections',
+		JSON.stringify({
+			message: 'A Server connected through Node.js SDK',
+		})
+	);
+	MqttBroker.subscribe('RAS/DataPoints');
+});
+
+MqttBroker.on('error', (error) => {
+	console.log('Error: AWS IoT Mqtt Broker closed unexpectedly');
+	MqttBroker = awsIoT.device({
+		keyPath: './_creds/private.pem.key',
+		certPath: './_creds/certificate.pem.crt',
+		caPath: './_creds/root-CA1.pem',
+		clientId: 'server',
+		host: `${process.env.AWS_IOT_HOST}.iot.${process.env.AWS_IOT_REGION}.amazonaws.com`,
+		region: 'ap-south-1',
+	});
+});
+
+module.exports = MqttBroker;
